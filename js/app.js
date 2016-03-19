df61238bc59f9b909d217e581737a45f720d2b22
Jason Smith <jsmith@ceigon.com>
2016-03-19 11:42:48 -0400

    cleaned it up a little

--- js/app.js
+++ js/app.js
@@ -11,61 +11,59 @@ String.prototype.matchAll = function(regexp) {
     return matches.length ? matches :null;
 };
 
-(function() {
-    var app = angular.module('dealerTrack', []);
-    app.controller("OutputController",function(){
-        this.output = "";
-        this.nonAlphaRegexp = /[^A-Za-z]/;
-        this.matchAllNonAlphaRegexp = /[^A-Za-z]/g;
-        this.words = [];
+var app = angular.module('dealerTrack', []);
+app.controller("OutputController",function(){
+    this.output = "";
+    this.nonAlphaRegexp = /[^A-Za-z]/;
+    this.matchAllNonAlphaRegexp = /[^A-Za-z]/g;
+    this.words = [];
+    this.modifiedWords = [];
+    this.delemiterLocationsInString = [];
+    this.delemiterInputsInString = [];
+
+    this.setOutput = function(output){
+        this.output = (output)?output:"";
+    };
+
+    this.displayOutput = function(){
+        var outputString = "";
+        var allNonAlphaCharsIndexArray = [];
+        var allNonAlphaCharsInputArray = [];
         this.modifiedWords = [];
-        this.delemiterLocationsInString = [];
-        this.delemiterInputsInString = [];
 
-        this.setOutput = function(output){
-            this.output = (output)?output:"";
-        };
+        if(this.output.matchAll(this.matchAllNonAlphaRegexp) ) {
+            var allNonAlphaChars = this.output.matchAll(this.matchAllNonAlphaRegexp);
+            for(i = 0; i < allNonAlphaChars.length; i++){
+                allNonAlphaCharsIndexArray.push(allNonAlphaChars[i].index);
+                allNonAlphaCharsInputArray.push(allNonAlphaChars[i][0]);
+            }
+            this.delemiterLocationsInString = allNonAlphaCharsIndexArray;
+            this.delemiterInputsInString = allNonAlphaCharsInputArray;
 
-        this.displayOutput = function(){
-            var outputString = "";
-            var allNonAlphaCharsIndexArray = [];
-            var allNonAlphaCharsInputArray = [];
-            this.modifiedWords = [];
-
-            if(this.output.matchAll(this.matchAllNonAlphaRegexp) ) {
-                var allNonAlphaChars = this.output.matchAll(this.matchAllNonAlphaRegexp);
-                for(i = 0; i < allNonAlphaChars.length; i++){
-                    allNonAlphaCharsIndexArray.push(allNonAlphaChars[i].index);
-                    allNonAlphaCharsInputArray.push(allNonAlphaChars[i][0]);
-                }
-                this.delemiterLocationsInString = allNonAlphaCharsIndexArray;
-                this.delemiterInputsInString = allNonAlphaCharsInputArray;
-
-                this.words = this.output.split(this.nonAlphaRegexp);
-                for(var i = 0; i < this.words.length; i++) {
-                    this.modifiedWords.push(this.modifyWord(this.words[i]));
-                }
-
-                for(i = 0; i < this.modifiedWords.length;i++){
-                    outputString += this.modifiedWords[i]+
-                        ((this.delemiterInputsInString[i])?this.delemiterInputsInString[i]:"");
-                }
+            this.words = this.output.split(this.nonAlphaRegexp);
+            for(var i = 0; i < this.words.length; i++) {
+                this.modifiedWords.push(this.modifyWord(this.words[i]));
             }
 
-            return outputString;
-        };
-        this.modifyWord = function(output){
-            var firstLetter =  output.substr(0,1);
-            var lastLetter  =  output.substr(output.length-1,output.length);
-            var count = output.length-2;
-            return (output.length > 0)?firstLetter+count+lastLetter:"";
-        };
-    });
-    //create <test-output> tag directive to display output
-    app.directive("testOutput",function(){
-        return {
-            restrict: 'E',
-            templateUrl: "output.html"
-        };
-    });
-})();
+            for(i = 0; i < this.modifiedWords.length;i++){
+                outputString += this.modifiedWords[i]+
+                    ((this.delemiterInputsInString[i])?this.delemiterInputsInString[i]:"");
+            }
+        }
+
+        return outputString;
+    };
+    this.modifyWord = function(output){
+        var firstLetter =  output.substr(0,1);
+        var lastLetter  =  output.substr(output.length-1,output.length);
+        var count = output.length-2;
+        return (output.length > 0)?firstLetter+count+lastLetter:"";
+    };
+});
+//create <test-output> tag directive to display output
+app.directive("testOutput",function(){
+    return {
+        restrict: 'E',
+        templateUrl: "output.html"
+    };
+});
